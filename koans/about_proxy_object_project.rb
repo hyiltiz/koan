e4d1f20b25006588270aef2f302bd8dc6f1a1c60
Hörmetjan Yiltiz <hyiltiz@gmail.com>
2016-07-22 14:03:27 -0400

    messaging is quite like eval, weird

--- koans/about_proxy_object_project.rb
+++ koans/about_proxy_object_project.rb
@@ -13,12 +13,28 @@ require File.expand_path(File.dirname(__FILE__) + '/neo')
 # of the Proxy class is given in the AboutProxyObjectProject koan.
 
 class Proxy
+  attr_accessor :messages
+
   def initialize(target_object)
     @object = target_object
     # ADD MORE CODE HERE
+    @messages = []
+  end
+
+  def called?(message)
+    @messages.include?(message)
+  end
+
+  def number_of_times_called(message)
+    @messages.count(message)
   end
 
   # WRITE CODE HERE
+  def method_missing(method_name, *args, &block)
+    @messages << method_name
+    @object.send(method_name, *args, &block)
+  end
+
 end
 
 # The proxy object should pass the following Koan:
